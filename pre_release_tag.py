ac316546c337e3f150b7a68c64ce2efada6f9308
meet <meet.luhar@jeavio.com>
2020-02-10 11:32:17 +0530

    Updates pre_release_tag.py

--- pre_release_tag.py
+++ pre_release_tag.py
@@ -22,6 +22,7 @@ def main():
             subprocess.run([cmd], shell=True, check=True)
             release_mesg=generate_release_mesg(last_git_tag)
             check_commit_id(last_git_tag)
+            gen_release_branch(release_version)
             gen_tag(release_version,release_mesg)
     except Exception as e:
         print("Exception occurred during application deployment :{}".format(e))
@@ -34,12 +35,12 @@ def fetch_release_version() :
     :return: release version name
     """
     gocd_label = os.environ['GO_PIPELINE_LABEL']
-    version = re.findall(r'([0-9]+.[0-9]+.[0-9]+).', gocd_label)  # To fetch the verison from gocd label.
+    version = re.findall(r'^([0-9]+.[0-9]+.[0-9]+).', gocd_label)  # To fetch the verison from gocd label.
     if not version:
         raise Exception('Could not fetch semantic version from the GoCD label. Please check the Release Label used during build.')
     if not version[0] :
         raise Exception('Could not fetch the version number from the GoCD label . Please check...')
-    return "release-{}".format(version[0])
+    return version[0]
 
 def check_last_tag(release_version):
     """
@@ -47,12 +48,9 @@ def check_last_tag(release_version):
     :param: release_version version to be released
     :return: last released tag
     """
-    cmd=" git ls-remote --tags origin | tail -1 | sed 's:.*/::' | egrep -o 'release-[0-9]+.[0-9]+.[0-9]+'"
+    cmd="git for-each-ref --sort=taggerdate --format '%(refname) %(taggerdate)' refs/tags/[0-9]* | tail -1 | grep -E -o '[0-9]+.[0-9]+.[0-9]+' | head -1"
     last_tag=commands_output(cmd)
     if last_tag == release_version :
-        # delete_tag_cmd = "git push --delete origin {}".format(release_version)
-        # subprocess.run([delete_tag_cmd], shell=True, check=True)
-        # check_last_tag(release_version)
         raise Exception("Tag name already initialize please check")
     return last_tag
 
@@ -62,13 +60,14 @@ def generate_release_mesg(last_git_tag):
     :param last_git_tag: last release version name to get the MR mesg
     :return: mesg to be pushed with the tags
     """
-    feature_release_cmd='git log --pretty="%h - %s (%an)" {}..HEAD | grep "Merge pull request" | grep -E -o "(bug).*$" | grep -E -o "(WEB).* " | uniq'.format(last_git_tag) #To fetch the feature release mesg
+
+    feature_release_cmd='git log --pretty="%h - %s (%an)" {}..HEAD | grep "Merge pull request" | grep -E -o "(feature).*$" | grep -E -o "(WEB).*" | sed "s/-/:/2" | uniq'.format(last_git_tag) #To fetch the feature release mesg
     feature_release_note=commands_output(feature_release_cmd)
-    bug_release_cmd='git log --pretty="%h - %s (%an)" {}..HEAD | grep "Merge pull request" | grep -E -o "(bug).*$" | grep -E -o "(WEB).* " | uniq'.format(last_git_tag) #To fetch the bug release mesg
+    bug_release_cmd='git log --pretty="%h - %s (%an)" {}..HEAD | grep "Merge pull request" | grep -E -o "(bug).*$" | grep -E -o "(WEB).*" | sed "s/-/:/2" | uniq'.format(last_git_tag) #To fetch the bug release mesg
     bug_release_note=commands_output(bug_release_cmd)
-    if not bug_release_note:
+    if not bug_release_note and feature_release_note:
         return "Feature release:- \n{}".format(feature_release_note)
-    elif not feature_release_note:
+    elif not feature_release_note and bug_release_note:
         return "Defects fixed:- \n{}".format(bug_release_note)
     elif not bug_release_note and not feature_release_note:
         return ('Empty release notes')
@@ -84,13 +83,16 @@ def check_commit_id(last_git_tag):
     master_commit_ID = commands_output(master_commit_ID_cmd)
     last_tag_commit_ID_cmd = "git rev-list -n 1 {}".format(last_git_tag)  # To get the last tag commit ID
     last_tag_commit_ID = commands_output(last_tag_commit_ID_cmd)
-    develop_commit_ID_cmd = "git rev-parse origin/develop"  # To get the develop commit ID.
-    develop_commit_ID = commands_output(develop_commit_ID_cmd)
-    print(master_commit_ID , last_tag_commit_ID, develop_commit_ID)
-    if not develop_commit_ID or not last_tag_commit_ID or not master_commit_ID:  # Null check for variables
+    if not last_tag_commit_ID or not master_commit_ID:  # Null check for variables
         raise Exception('Commit id not found')
-    if master_commit_ID == last_tag_commit_ID or develop_commit_ID != master_commit_ID:  # Check the commit id
+    if master_commit_ID == last_tag_commit_ID :  # Check the commit id
         raise Exception('No changes detected as compared to latest release. Please check for any pending MR to master and try again.')
+
+def gen_release_branch(release_version):
+    release_branch_cmd="git checkout -b release-{}".format(release_version)
+    subprocess.run([release_branch_cmd], shell=True, check=True)
+    release_branch_push_cmd="git push origin release-{}".format(release_branch_cmd)
+    subprocess.run([release_branch_push_cmd], shell=True , check=True)
 
 def gen_tag(release_version,release_mesg):
     """
